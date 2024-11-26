d49dc716fb32818141ca611a8e668dde38ef5c77
Preslav977 <p.cvetanow@gmail.com>
2024-11-26 13:09:05 +0200

    Added more routes to folderRouter to handle file CRUD logic by fileController

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -6,6 +6,8 @@ const folderController = require("../controllers/folderController");
 
 const subfolderController = require("../controllers/subfolderController");
 
+const fileController = require("../controllers/fileController");
+
 folderRouter.get("/create", folderController.folder_create_get);
 
 folderRouter.post("/create", folderController.folder_create_post);
@@ -14,15 +16,33 @@ folderRouter.get("/create/:id", subfolderController.subfolder_create_get);
 
 folderRouter.post("/create/:id", subfolderController.subfolder_create_post);
 
+folderRouter.get("/upload/:id", fileController.file_upload_file_get);
+
+folderRouter.post("/upload/:id", fileController.file_upload_file_post);
+
+folderRouter.post(
+  "/:id/download/:fileId",
+  fileController.file_download_file_post
+);
+
 folderRouter.get("/delete/:id", subfolderController.subfolder_delete_get);
 
 folderRouter.post("/delete/:id", subfolderController.subfolder_delete_post);
+
+folderRouter.post("/:id/delete/:fileId", fileController.file_delete_post);
 
 folderRouter.get("/share/:id", subfolderController.subfolder_share_get);
 
 folderRouter.post("/share/:id", subfolderController.subfolder_share_post);
 
 folderRouter.get("/:id", folderController.folder_details);
+
+folderRouter.get("/:id/file/:fileId", fileController.file_details_get);
+
+folderRouter.get(
+  "/shared/:sharedLink",
+  subfolderController.subfolder_shared_details
+);
 
 folderRouter.get("/", folderController.folders_get);
 
