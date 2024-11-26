c645de77f1992d525e7c9a0b352b33217932dd65
Preslav977 <p.cvetanow@gmail.com>
2024-11-26 13:11:15 +0200

    Created middleware to validate file

---
+++ middlewares/validateFile.js
@@ -0,0 +1,32 @@
+const { check } = require("express-validator");
+
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const formatFileSize = require("../helper/formatFileSize");
+
+const fileNameTaken = "is already taken";
+
+const fileSizeExceedLimit = "size exceed limit";
+
+validateFile = [
+  check("uploaded_file").custom(async (value, { req }) => {
+    const fileUploadSize = formatFileSize(req.file.size);
+
+    if (fileUploadSize > "5.00 MB") {
+      throw new Error(`File ${fileSizeExceedLimit}`);
+    }
+
+    const fileExists = await prisma.file.findFirst({
+      where: {
+        name: req.file.originalname,
+      },
+    });
+    if (fileExists) {
+      throw new Error(`File name ${fileNameTaken}`);
+    }
+  }),
+];
+
+module.exports = validateFile;
