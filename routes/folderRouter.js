1a4e52f4fe6e49ef40105680c5a32b1c566249e6
Preslav977 <p.cvetanow@gmail.com>
2024-11-26 13:02:59 +0200

    Added to folderRouter necessary routes to handle CRUD actions for subfolders

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -4,9 +4,23 @@ const folderRouter = Router();
 
 const folderController = require("../controllers/folderController");
 
+const subfolderController = require("../controllers/subfolderController");
+
 folderRouter.get("/create", folderController.folder_create_get);
 
 folderRouter.post("/create", folderController.folder_create_post);
+
+folderRouter.get("/create/:id", subfolderController.subfolder_create_get);
+
+folderRouter.post("/create/:id", subfolderController.subfolder_create_post);
+
+folderRouter.get("/delete/:id", subfolderController.subfolder_delete_get);
+
+folderRouter.post("/delete/:id", subfolderController.subfolder_delete_post);
+
+folderRouter.get("/share/:id", subfolderController.subfolder_share_get);
+
+folderRouter.post("/share/:id", subfolderController.subfolder_share_post);
 
 folderRouter.get("/:id", folderController.folder_details);
 
