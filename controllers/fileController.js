bfb968a8e6feda80653822213f858a55141d6c6f
Preslav977 <p.cvetanow@gmail.com>
2024-11-26 13:18:06 +0200

    Created the controller that will handle uploading a file with validateFile middleware to validate the file

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -69,3 +69,38 @@ function runMiddleware(req, res, fn) {
     });
   });
 }
+
+let cloudinaryResponse;
+
+exports.file_upload_file_post = [
+  asyncHandler(async (req, res, next) => {
+    await runMiddleware(req, res, multerUploadMiddleware);
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+    cloudinaryResponse = await handleFileUpload(dataURI);
+    next();
+  }),
+
+  validateFile,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const { id } = req.params;
+
+    if (!errors.isEmpty()) {
+      res.status(400).send(errors.array());
+    } else {
+      const uploadFile = await prisma.file.create({
+        data: {
+          name: req.file.originalname,
+          URL: cloudinaryResponse.secure_url,
+          size: formatFileSize(req.file.size),
+          createdAt: new Date(),
+          userId: req.user.id,
+          folderId: Number(id),
+        },
+      });
+      console.log(uploadFile);
+    }
+  },
+];
