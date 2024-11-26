f87680dd95a8f5c36df00baf904a765fdddef3ab
Preslav977 <p.cvetanow@gmail.com>
2024-11-26 12:54:43 +0200

    Created the userController with logic to handle user registrating and log in logic

---
+++ controllers/userController.js
@@ -0,0 +1,48 @@
+const { validationResult } = require("express-validator");
+
+const bcrypt = require("bcryptjs");
+
+const asyncHandler = require("express-async-handler");
+
+const validateUser = require("../middlewares/validateUser");
+
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+exports.user_sign_up_get = asyncHandler(async (req, res, next) => {
+  res.render("sign-up");
+});
+
+exports.user_sign_up_post = [
+  validateUser,
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const { username, password, confirm_password } = req.body;
+
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
+      if (err) {
+        console.error("Failed to hash the passwords", err);
+        throw err;
+      }
+      if (!errors.isEmpty()) {
+        res.status(400).send(errors.array());
+      } else {
+        const createUser = await prisma.user.create({
+          data: {
+            username: username,
+            password: hashedPassword,
+            confirm_password: hashedPassword,
+          },
+        });
+
+        res.redirect("/");
+      }
+    });
+  }),
+];
+
+exports.user_log_in_get = asyncHandler(async (req, res, next) => {
+  res.render("log-in");
+});
