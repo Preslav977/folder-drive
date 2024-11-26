831e6d6413cd368d6d1216dbaeb0bac3344162c2
Preslav977 <p.cvetanow@gmail.com>
2024-11-26 13:13:05 +0200

    Created controller to get file details

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -9,3 +9,25 @@ const prisma = new PrismaClient();
 const cloudinary = require("cloudinary").v2;
 
 const upload = require("../middlewares/multer");
+
+const validateFile = require("../middlewares/validateFile");
+
+const formatFileSize = require("../helper/formatFileSize");
+
+exports.file_details_get = asyncHandler(async (req, res, next) => {
+  const { id, fileId } = req.params;
+
+  const fileDetails = await prisma.file.findUnique({
+    where: {
+      id: Number(fileId),
+    },
+    include: {
+      folder: Number(id),
+    },
+  });
+  // console.log(fileDetails);
+
+  res.render("index", {
+    file: fileDetails,
+  });
+});
