da56f659a3a560f5c5e4b90163b0321f4cf2a728
Preslav977 <p.cvetanow@gmail.com>
2024-11-26 13:04:05 +0200

    Created controllers to create subfolders with GET and POST

---
+++ controllers/subfolderController.js
@@ -0,0 +1,56 @@
+const asyncHandler = require("express-async-handler");
+
+const validateFolder = require("../middlewares/validateFolder");
+
+const { PrismaClient } = require("@prisma/client");
+
+const { validationResult } = require("express-validator");
+
+const prisma = new PrismaClient();
+
+const { v4: uuidv4 } = require("uuid");
+
+exports.subfolder_create_get = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+
+  const getParentFolder = await prisma.folder.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  // console.log(getParentFolder);
+
+  res.render("create-subfolder", {
+    folder: getParentFolder,
+  });
+});
+
+exports.subfolder_create_post = [
+  validateFolder,
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const { id } = req.params;
+
+    const { name } = req.body;
+
+    if (!errors.isEmpty()) {
+      res.status(400).send(errors.array());
+    } else {
+      const createNestedFolder = await prisma.folder.create({
+        data: {
+          name: name,
+          size: "--",
+          createdAt: new Date(),
+          userId: req.user.id,
+          parentId: Number(id),
+        },
+      });
+
+      console.log(createNestedFolder);
+
+      res.redirect(`/folders/${id}`);
+    }
+  }),
+];
